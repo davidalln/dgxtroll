8e2d785b30e4ee116e4579793494afc9034f94e4
David Allen <davialln@gmail.com>
2022-08-26 20:18:04 -0400

    ui: sectioned off part voice selector

--- app/dgx.js
+++ app/dgx.js
@@ -4,13 +4,18 @@ const fs = require("fs")
 const pug = require("pug")
 const JZZ = require("jzz")
 
+// config JSON files
 const fConfigDgxPgmData = fs.readFileSync(path.join(__dirname, "config", "dgx_pgm_data.json"), { encoding: "utf8" })
-compiledConfigDgxPgmData = JSON.parse(fConfigDgxPgmData)
+cConfigDgxPgmData = JSON.parse(fConfigDgxPgmData)
 
+// PUG components
 const fUIMidi = fs.readFileSync(path.join(__dirname, "views", "components", "midi.pug"), { encoding: "utf8" })
-const fUIVoice = fs.readFileSync(path.join(__dirname, "views", "components", "part.pug"), { encoding: "utf8" })
-const compiledUIMidi = pug.compile(fUIMidi)
-const compiledUIPart = pug.compile(fUIVoice)
+const fUIPart = fs.readFileSync(path.join(__dirname, "views", "components", "part.pug"), { encoding: "utf8" })
+const fUIPart_VoiceSelector = fs.readFileSync(path.join(__dirname, "views", "components", "part_voice-selector.pug"), { encoding: "utf8" })
+
+const cUIMidi = pug.compile(fUIMidi)
+const cUIPart = pug.compile(fUIPart)
+const cUIPart_VoiceSelector = pug.compile(fUIPart_VoiceSelector)
 
 
 const midi = {
@@ -60,9 +65,6 @@ function createWindow () {
   // ---- IPC CONFIG ----
   // send configuration data to controller
   ipcMain.handle("config:get-max-parts", async (_) => { return 4 })
-  ipcMain.handle("config:get-dgx-program-data", async (_) => {
-    return compiledConfigDgxPgmData
-  })
 
   // ---- IPC UI ----
   // send rendered pug ui components
@@ -73,12 +75,17 @@ function createWindow () {
       outputs.push(JZZ.info().outputs[i].name)
     }
 
-    return compiledUIMidi({ outputs: outputs }) 
+    return cUIMidi({ outputs: outputs }) 
   })
+
   ipcMain.handle("ui:get-rendered-part", async (_, id) => {
-    return compiledUIPart({ 
+    return cUIPart({ id: id })
+  })
+
+  ipcMain.handle("ui:get-rendered-part_voice-selector", async (_, id) => {
+    return cUIPart_VoiceSelector({ 
       id: id, 
-      pgm_data: compiledConfigDgxPgmData, 
+      pgm_data: cConfigDgxPgmData, 
       default_category: 1 }
     ) 
   })
--- app/preload.js
+++ app/preload.js
@@ -2,10 +2,10 @@ const { contextBridge, ipcRenderer } = require("electron")
 
 contextBridge.exposeInMainWorld("dgxAPI", {
   maxParts: () => ipcRenderer.invoke("config:get-max-parts"),
-  dgxPgmData: () => ipcRenderer.invoke("config:get-dgx-program-data"),
 
   renderedMidi: (id) => ipcRenderer.invoke("ui:get-rendered-midi"),
   renderedPart: (id) => ipcRenderer.invoke("ui:get-rendered-part", id),
+  renderedPart_VoiceSelector: (id) => ipcRenderer.invoke("ui:get-rendered-part_voice-selector", id),
 
   getMIDIOutputs: () => ipcRenderer.invoke("midi:get-outputs")
 })
